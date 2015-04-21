b738454f171dc0eca1fcb5bb0e5a28dd48d209d3
Paulsen J Siahaan <paulsen.s@samsung.com>
2015-04-21 13:45:50 +0700

    chocolatefeast

--- HackerRank/src/chocolatefeast/Solution.java
+++ HackerRank/src/chocolatefeast/Solution.java
@@ -7,7 +7,6 @@ public class Solution {
 	static int intInput;
 	static long ANSWER=0;
 	static int N, C, M;
-	static int _donuts = 0;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -19,27 +18,20 @@ public class Solution {
 			C = sc.nextInt();
 			M = sc.nextInt();
 			
-			if ((N / C) == M) {
-				ANSWER = M+1;
-			}else if ((N / C) < M) {
-				ANSWER = (N / C);
-			}else{
-				ANSWER = (N / C) + iterateWrappers((N / C), M);
-			}
+			ANSWER = (N / C) + countWrappers((N / C), M);
 			
 			System.out.println(ANSWER);
 			ANSWER=0;
 		}
 	}
 	
-	static int iterateWrappers(int wrappers, int changed) {
-		if (wrappers < changed) {
-			return _donuts;
-		}else {
-			if (wrappers % changed != 0) {
-				_donuts += (Math.floor(wrappers / changed) + (wrappers - changed))/2; 
-			}
+	static int countWrappers(int wrappers, int changed) {
+		int _donuts = 0;
+		while (wrappers / changed > 0) {
+			_donuts += wrappers / changed;
+			wrappers = (wrappers / M) + (wrappers % M);
 		}
-		return iterateWrappers(_donuts, changed);
+		
+		return _donuts;
 	}
 }
